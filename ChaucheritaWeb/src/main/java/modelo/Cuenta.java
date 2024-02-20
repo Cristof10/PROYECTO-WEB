d337dd83159923c34930ef9ad436c529a8d17c96
Nesss <89305157+desnes@users.noreply.github.com>
2024-02-20 12:58:47 -0500

    add jsp implementation

--- ChaucheritaWeb/src/main/java/modelo/Cuenta.java
+++ ChaucheritaWeb/src/main/java/modelo/Cuenta.java
@@ -2,9 +2,20 @@ package modelo;
 
 import java.util.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name="Cuenta")
 public class Cuenta {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "valor")
+	private String nombre;
+	
 
     public Cuenta() {
     }
--- ChaucheritaWeb/src/main/java/modelo/Ingreso.java
+++ ChaucheritaWeb/src/main/java/modelo/Ingreso.java
@@ -8,10 +8,60 @@ public class Ingreso implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Double valor;
 	private String concepto;
-	private Cuenta cuenta;
+	private Cuenta cuentaOrigen;
+	private Categoria categoria;
+	private Date fecha;
 
 	public Ingreso() {
     	
     }
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getConcepto() {
+		return concepto;
+	}
+
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+
+	public Cuenta getCuentaOrigen() {
+		return cuentaOrigen;
+	}
+
+	public void setCuentaOrigen(Cuenta cuentaOrigen) {
+		this.cuentaOrigen = cuentaOrigen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingreso [valor=" + this.valor + ", concepto=" + this.concepto + ", cuentaOrigen=" + this.cuentaOrigen + ", categoria="
+				+ this.categoria + ", fecha=" + this.fecha + "]";
+	}
+
+	
 
 }
